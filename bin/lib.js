8e06be4c7c385634f08ff6f8299530bbc9159e5d
tjoskar <hello@tjoskar.dev>
2022-07-17 17:39:28 +0200

    chore: upgrade to module
    Upgrade packages and use modules

--- bin/lib.js
+++ bin/lib.js
@@ -1,14 +1,14 @@
-'use strict';
+import {writeFile, readFile, chmod} from 'node:fs/promises';
+import process from 'node:process';
+import inquirer from 'inquirer';
+import fetch from 'node-fetch';
+import chalk from 'chalk';
+import {pathExists} from 'path-exists';
+import fileExists from 'file-exists';
 
-const { writeFile, readFile, chmod } = require('fs').promises;
-const inquirer = require('inquirer');
-const fetch = require('node-fetch');
-const chalk = require('chalk');
-const pathExists = require('path-exists');
-const fileExists = require('file-exists');
-
-const gitmojiUrl = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json';
-const prepareCommitMsgFileName = 'prepare-commit-msg';
+const gitmojiUrl
+  = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json';
+const prepareCommitMessageFileName = 'prepare-commit-msg';
 
 const gitmojiCommitHookComand = `#!/bin/sh
 exec < /dev/tty
@@ -16,134 +16,151 @@ gitmoji-commit-hook $1
 `;
 
 const errorMessage = {
-  notGit: 'The directory is not a git repository.',
-  commitHookExist: `A prepare-commit hook already exists, please remove the hook (rm .git/hooks/${prepareCommitMsgFileName}) or install gitmoji-commit-hook manually by adding the following content info .git/hooks/\n\n${prepareCommitMsgFileName}:${gitmojiCommitHookComand}`,
-  gitmojiParse: `Could not find gitmojis at ${gitmojiUrl}.`
+	notGit: 'The directory is not a git repository.',
+	commitHookExist: `A prepare-commit hook already exists, please remove the hook (rm .git/hooks/${prepareCommitMessageFileName}) or install gitmoji-commit-hook manually by adding the following content info .git/hooks/\n\n${prepareCommitMessageFileName}:${gitmojiCommitHookComand}`,
+	gitmojiParse: `Could not find gitmojis at ${gitmojiUrl}.`,
 };
 
 function errorHandler(error) {
-  console.error(chalk.red(`🚨  ERROR: ${error}`));
-  process.exit(1);
+	console.error(chalk.red(`🚨  ERROR: ${error}`));
+	process.exit(1);
 }
 
-function rejectIf(errorMsg) {
-  return val => val ? Promise.reject(new Error(errorMsg)) : val;
+function rejectIf(errorMessage_) {
+	return value => (value ? Promise.reject(new Error(errorMessage_)) : value);
 }
 
-function rejectIfNot(errorMsg) {
-  return val => val ? val : Promise.reject(new Error(errorMsg));
+function rejectIfNot(errorMessage_) {
+	return value => (value ? value : Promise.reject(new Error(errorMessage_)));
 }
 
 async function getGitmojiList() {
-  try {
-    const { gitmojis } = await fetch(gitmojiUrl).then(r => r.json());
-    if (!gitmojis) {
-      throw new Error(errorMessage.gitmojiParse);
-    }
-    return gitmojis;
-  } catch (error) {
-    throw new Error(errorMessage.gitmojiParse + ` ${error.message}`);
-  }
+	try {
+		const {gitmojis} = await fetch(gitmojiUrl).then(r => r.json());
+		if (!gitmojis) {
+			throw new Error(errorMessage.gitmojiParse);
+		}
+
+		return gitmojis;
+	} catch (error) {
+		throw new Error(errorMessage.gitmojiParse + ` ${error.message}`);
+	}
 }
 
 function assertGitRepository() {
-  return pathExists('.git')
-    .then(rejectIfNot(errorMessage.notGit));
+	return pathExists('.git').then(rejectIfNot(errorMessage.notGit));
 }
 
 function assertNoPrepareCommitHook(gitHookPath) {
-  return () => fileExists(`${gitHookPath}/${prepareCommitMsgFileName}`)
-    .then(rejectIf(errorMessage.commitHookExist));
+	return () =>
+		fileExists(`${gitHookPath}/${prepareCommitMessageFileName}`).then(
+			rejectIf(errorMessage.commitHookExist),
+		);
 }
 
 function initProject(gitHookPath) {
-  return assertGitRepository()
-    .then(assertNoPrepareCommitHook(gitHookPath))
-    .then(() => writeFile(`${gitHookPath}/${prepareCommitMsgFileName}`, gitmojiCommitHookComand))
-    .then(() => chmod(`${gitHookPath}/${prepareCommitMsgFileName}`, '755'));
+	return assertGitRepository()
+		.then(assertNoPrepareCommitHook(gitHookPath))
+		.then(() =>
+			writeFile(
+				`${gitHookPath}/${prepareCommitMessageFileName}`,
+				gitmojiCommitHookComand,
+			),
+		)
+		.then(() => chmod(`${gitHookPath}/${prepareCommitMessageFileName}`, '755'));
 }
 
 function prependMessage(getMessage, putMessage) {
-  return filepath => message => getMessage(filepath)
-    .then(fileContent => `${message} ${fileContent}`)
-    .then(fileContent => putMessage(filepath, fileContent));
+	return filepath => message =>
+		getMessage(filepath)
+			.then(fileContent => `${message} ${fileContent}`)
+			.then(fileContent => putMessage(filepath, fileContent));
 }
 
 const prependMessageToFile = prependMessage(readFile, writeFile);
 
-function getGitmojiBlacklist() {
-  return fileExists(`${process.env.PWD}/package.json`)
-    .then(exist => exist ? require(`${process.env.PWD}/package.json`) : {})
-    .then(packageJson => packageJson.gitmoji || {})
-    .then(gitmoji => gitmoji.blacklist || []);
+async function readJson(path) {
+	return JSON.parse(await readFile(path));
+}
+
+function getGitmojiExclude() {
+	return fileExists(`${process.env.PWD}/package.json`)
+		.then(exist => (exist ? readJson(`${process.env.PWD}/package.json`) : {}))
+		.then(packageJson => packageJson.gitmoji || {})
+		.then(gitmoji => gitmoji.blacklist || []);
 }
 
 function seperateChoices(choices) {
-  return blacklist => {
-    if (blacklist.length === 0) {
-      return choices;
-    }
-    return [
-      ...choices.filter(choice => !blacklist.includes(choice.type)),
-      new inquirer.Separator(),
-      ...choices.filter(choice => blacklist.includes(choice.type)),
-      new inquirer.Separator()
-    ];
-  };
+	return exclude => {
+		if (exclude.length === 0) {
+			return choices;
+		}
+
+		return [
+			...choices.filter(choice => !exclude.includes(choice.type)),
+			new inquirer.Separator(),
+			...choices.filter(choice => exclude.includes(choice.type)),
+			new inquirer.Separator(),
+		];
+	};
 }
 
-function seperateBlacklistEmojis(choices) {
-  return getGitmojiBlacklist()
-    .then(seperateChoices(choices));
+function seperateExcludeEmojis(choices) {
+	return getGitmojiExclude().then(seperateChoices(choices));
 }
 
 function printInitSuccess() {
-  console.log(`${chalk.green('🎉  SUCCESS 🎉')}  gitmoji-commit-hook initialized with success.`);
+	console.log(
+		`${chalk.green(
+			'🎉  SUCCESS 🎉',
+		)}  gitmoji-commit-hook initialized with success.`,
+	);
 }
 
 function mapGitmojiItemToOption(gitmoji) {
-  return {
-    name: gitmoji.emoji + '  ' + gitmoji.description,
-    value: gitmoji.emoji,
-    type: gitmoji.name
-  };
+	return {
+		name: gitmoji.emoji + '  ' + gitmoji.description,
+		value: gitmoji.emoji,
+		type: gitmoji.name,
+	};
 }
 
 function createInquirerQuestion(emojis) {
-  return [{
-    type: 'checkbox',
-    name: 'emoji',
-    message: 'Select emoji(s) for your commit',
-    choices: emojis
-  }];
+	return [
+		{
+			type: 'checkbox',
+			name: 'emoji',
+			message: 'Select emoji(s) for your commit',
+			choices: emojis,
+		},
+	];
 }
 
-const isCommitEditMsgFile = stringToTest => /COMMIT_EDITMSG/g.test(stringToTest);
+const isCommitEditMessageFile = stringToTest =>
+	/COMMIT_EDITMSG/g.test(stringToTest);
 
 function gitmojiCommitHook(gitHookPath, commitFile) {
-  if (commitFile === '--init') {
-    initProject(gitHookPath)
-      .then(printInitSuccess)
-      .catch(errorHandler);
-  } else if (isCommitEditMsgFile(commitFile)) {
-    getGitmojiList()
-      .then(emojis => emojis.map(mapGitmojiItemToOption))
-      .then(seperateBlacklistEmojis)
-      .then(createInquirerQuestion)
-      .then(inquirer.prompt)
-      .then(answers => answers.emoji)
-      .then(answers => answers.join(' '))
-      .then(prependMessageToFile(commitFile))
-      .catch(errorHandler);
-  }
+	if (commitFile === '--init') {
+		initProject(gitHookPath).then(printInitSuccess).catch(errorHandler);
+	} else if (isCommitEditMessageFile(commitFile)) {
+		getGitmojiList()
+			.then(emojis => emojis.map(emoji => mapGitmojiItemToOption(emoji)))
+			.then(seperateExcludeEmojis)
+			.then(createInquirerQuestion)
+			.then(inquirer.prompt)
+			.then(answers => answers.emoji)
+			.then(answers => answers.join(' '))
+			.then(prependMessageToFile(commitFile))
+			.catch(errorHandler);
+	}
 }
 
-module.exports = {
-  rejectIf,
-  rejectIfNot,
-  gitmojiCommitHook,
-  prependMessage,
-  mapGitmojiItemToOption,
-  createInquirerQuestion,
-  seperateChoices
+export {
+	rejectIf,
+	rejectIfNot,
+	gitmojiCommitHook,
+	prependMessage,
+	mapGitmojiItemToOption,
+	createInquirerQuestion,
+	seperateChoices,
 };
---
+++ bin/lib.test.js
@@ -0,0 +1,116 @@
+import {describe, test, expect} from 'vitest';
+import {spy} from 'simple-spy';
+import {
+	rejectIf,
+	rejectIfNot,
+	prependMessage,
+	mapGitmojiItemToOption,
+	createInquirerQuestion,
+	seperateChoices,
+} from './lib.js';
+
+describe('rejectIf', () => {
+	test('reject', () => {
+		const result = rejectIf('my error message')(true);
+
+		return result.catch(error => {
+			expect(error).toBeDefined();
+		});
+	});
+
+	test('do not reject', () => {
+		const result = rejectIf('my error message')(false);
+
+		expect(result).toBe(false);
+	});
+});
+
+describe('rejectIfNot', () => {
+	test('do not reject', () => {
+		const result = rejectIfNot('my error message')(true);
+
+		expect(result).toBe(true);
+	});
+
+	test('reject', () => {
+		const result = rejectIfNot('my error message')(false);
+
+		return result.catch(error => {
+			expect(error).toBeDefined();
+		});
+	});
+});
+
+test('Prepend a message', () => {
+	const getMessage = spy(() => Promise.resolve('World'));
+	const putMessage = spy(() => Promise.resolve());
+	const fileName = 'myfile.txt';
+	const message = 'Hello';
+
+	return prependMessage(
+		getMessage,
+		putMessage,
+	)(fileName)(message).then(() => {
+		expect(getMessage.args[0][0]).toBe(fileName);
+		expect(putMessage.args[0][0]).toBe(fileName);
+		expect(putMessage.args[0][1]).toBe('Hello World');
+	});
+});
+
+test('Map gitmoji item to an option', () => {
+	const gitmoji = {
+		emoji: 'a',
+		description: 'Something awesome',
+	};
+
+	const result = mapGitmojiItemToOption(gitmoji);
+
+	expect(result.value).toEqual('a');
+	expect(result.name).toEqual('a  Something awesome');
+});
+
+test('Create Inquirer question', () => {
+	const choices = [
+		{
+			name: 'Something',
+			value: 's',
+		},
+	];
+	const result = createInquirerQuestion(choices);
+
+	expect(result[0].choices).toBe(choices);
+});
+
+test('Seperate choices', () => {
+	const choices = [
+		{
+			type: ':cat:',
+		},
+		{
+			type: ':dog:',
+		},
+	];
+	const exclude = [':dog:'];
+
+	const result = seperateChoices(choices)(exclude);
+
+	expect(result[0]).toEqual({type: ':cat:'});
+	expect(result[1].type).toEqual('separator');
+	expect(result[2]).toEqual({type: ':dog:'});
+});
+
+test('Return same list if no blacklist', () => {
+	const choices = [
+		{
+			type: ':cat:',
+		},
+		{
+			type: ':dog:',
+		},
+	];
+	const exclude = [];
+
+	const result = seperateChoices(choices)(exclude);
+
+	expect(result).toBe(choices);
+});
